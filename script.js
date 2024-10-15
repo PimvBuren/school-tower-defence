ed2d5ef45201c81b51ff89f531522c43866877c4
PimvBuren <6043915@mborijnland.nl>
2024-10-15 13:34:26 +0200

    pseudocode

---
+++ script.js
@@ -0,0 +1,24 @@
+// hier komt de loopaden van met de grenzen en waar je kan lopen als leerling
+// hier komt te staan waar je het docent kan plaatsen
+// hier komt de coin system te staan
+// hier komen de healh bar te staan van het hek 
+// hier komt de damage van de docent te staan hoeveel ze aanrichten
+// hier komt te staan hoeveel health de leerlingen hebben
+// hier komt de damage van de leerlingen
+           // hier komt de snelheid van de leerlingen te staan
+                // hier komt te staan dat elke ronde ze sterker worden
+// hier komt te staan dat je op het docent kan klikken en upgraden docent 1
+            // hier komt ook het docent slepen
+// hier komt te staan dat je op het docent kan klikken en upgraden docent 2
+            // hier komt ook het docent slepen
+// hier komt de mini boss 1 te staan die bij wave 10 komt
+            // special ability snelheid
+// hier komt de mini boss 2 te staan die bij wave 20 komt
+            //  special ability stunn
+// systeem dat je opnieuw begint als je af bent door op de knop te drukken
+// start knop om te beginnen
+// waves aangeven
+// tijd aangeven
+// units places / docenten
+// gems 
+//  damage done
